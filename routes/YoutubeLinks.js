a07b1318241c027ba003eeec432dbdfa4dbe729e
Steven Rago <stevenrago@stevens-mini.attlocal.net>
2017-06-01 00:10:25 -0500

    now using multiple browsers in function that gets links | faster to get links

--- routes/YoutubeLinks.js
+++ routes/YoutubeLinks.js
@@ -6,32 +6,33 @@ const DownloadLinks = require('../src/DownloadLinks')
 const youtubeApiString = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyC1YXfs9bkaCnpWnD4XIGl-jsDV3S8RAcM'
 
 const processYoutubeData = function(youtubeData){
-  console.log(youtubeData.items)
 
   const videoIDs = youtubeData.items.map((e)=>{
     return {id:e.id.videoId,
             title: e.snippet.title}
   })
 
+  console.log(videoIDs)
   return videoIDs
 }
 
 
 router.get('/', (req, res, next) => {
   const searchQuery = req.query.q
+  console.log('Search QUery:',searchQuery)
   let output = {}
 
 
-  axios.get(youtubeApiString + '&part=snippet&type=video&q=get%20back&maxResults=1')
+  axios.get(youtubeApiString + '&part=snippet&type=video&q='+ searchQuery +'&maxResults=2')
   .then((response) => {
     const links = processYoutubeData(response['data'])
     output.links = links 
+
 
     DownloadLinks.fetchDownloadLinks(links,(returnData)=>{
       res.send(returnData)
     })
 
-   // res.send(output)
 
 
   })
--- src/DownloadLinks.js
+++ src/DownloadLinks.js
@@ -1,37 +1,86 @@
 //var selenium = require('selenium-webdriver');
 var webdriver = require('selenium-webdriver');
+let browser = {browser: new webdriver.Builder().usingServer().withCapabilities({'browserName': 'chrome' }).build(),
+                 inUse:false};
+let browser2 = {browser: new webdriver.Builder().usingServer().withCapabilities({'browserName': 'chrome' }).build(),
+                 inUse:false};
+
+var count = 0
+
+var browsers = [ browser , browser2]
 
 const fetchDownloadLinks = function getData(youtubeArray, cb){
-  const youtubeLink = 'https://www.youtube.com/watch?v=' + youtubeArray[0].id
+  
 
-  var browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'chrome' }).build();
- 
-  browser.get('https://www.ytmp3.cc')
+  youtubeArray.forEach((id_title_downloadLink)=>{
+    //let haveBrowser = false
+    var iid = setInterval(()=>{
+      for(let x = 0; x<browsers.length ; x++){
+        if(browsers[x].inUse == false){
+          console.log('using browser', id_title_downloadLink.id )
+          browsers[x].inUse = true
+          console.log('using browser set', id_title_downloadLink.id)
+          clearInterval(iid)
 
-  browser.findElement(webdriver.By.id('input')).sendKeys(youtubeLink).then((inputField)=>{
-    console.log(inputField)
+          //DO STUFF
+              const youtubeLink = 'https://www.youtube.com/watch?v=' + id_title_downloadLink.id
 
-    browser.findElement(webdriver.By.id('submit')).click()
+    browsers[x].browser.get('https://www.ytmp3.cc')
+    browsers[x].browser.findElement(webdriver.By.id('input')).sendKeys(youtubeLink).then((inputField)=>{
+      //console.log(inputField)
 
-   var refreshID = setInterval(()=>{
+      browsers[x].browser.findElement(webdriver.By.id('submit')).click()
 
-       browser.findElement(webdriver.By.id('file')).getAttribute("href").then((data)=>{
-        //console.log(data)
-        clearInterval(refreshID)
-        browser.quit()
-        youtubeArray[0].downloadLink = data
-        cb(youtubeArray)
-       })
-       .catch((err)=>{
-        console.log(err)
-       })
+    var refreshID = setInterval(()=>{
 
-      
-    }, 2000)
+        browsers[x].browser.findElement(webdriver.By.id('file')).getAttribute("href").then((data)=>{
+          console.log('got link - :', data)
+          clearInterval(refreshID)
+          browsers[x].inUse = false
+          id_title_downloadLink.downloadLink = data
+          let checkArray = youtubeArray.map((e)=>{
+              if(typeof e.downloadLink !== 'undefined')
+                return 1 
+              else 
+                return 0
+          }).reduce((acc,curr)=>{
+            return acc+curr
+          },0)
+          console.log('procssed:', checkArray)
+          if(checkArray == youtubeArray.length){
+            cb(youtubeArray)
+          }
+          else{
+            
+            //DO NOTHING. youtubedata.downloadLink needs to be updated for all
+          }
+
+          
+        })
+        .catch((err)=>{
+          console.log(err)
+        })
+
+      }, 2000)
+
+    })
+    break;
+
+          
+        }
+        else{
+          //console.log('browser in use')
+          //IF browser is in use, do nothing
+        }
+
+      }
+    },1000)
+
 
 
   })
 
 }
+          //browser.quit()
 
 module.exports = {fetchDownloadLinks};
